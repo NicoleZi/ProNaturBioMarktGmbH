ad821dc018a8d6653c0042cc07bc8ef14881e29a
NicoleZi <colle888@aol.com>
2024-01-30 13:43:38 +0100

    Projektdateien hinzufügen.

---
+++ ProNaturBioMarktGmbH/LoadingScreen.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ProNaturBioMarktGmbH
+{
+    partial class LoadingScreen
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingScreen));
+            this.progressBarLoading = new System.Windows.Forms.ProgressBar();
+            this.labelLoadingText = new System.Windows.Forms.Label();
+            this.labelProcent = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadingbarTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBarLoading
+            // 
+            this.progressBarLoading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarLoading.Location = new System.Drawing.Point(12, 291);
+            this.progressBarLoading.Name = "progressBarLoading";
+            this.progressBarLoading.Size = new System.Drawing.Size(660, 25);
+            this.progressBarLoading.TabIndex = 0;
+            // 
+            // labelLoadingText
+            // 
+            this.labelLoadingText.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.labelLoadingText.AutoSize = true;
+            this.labelLoadingText.ForeColor = System.Drawing.Color.White;
+            this.labelLoadingText.Location = new System.Drawing.Point(279, 275);
+            this.labelLoadingText.Name = "labelLoadingText";
+            this.labelLoadingText.Size = new System.Drawing.Size(54, 13);
+            this.labelLoadingText.TabIndex = 1;
+            this.labelLoadingText.Text = "Loading...";
+            // 
+            // labelProcent
+            // 
+            this.labelProcent.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.labelProcent.AutoSize = true;
+            this.labelProcent.ForeColor = System.Drawing.Color.White;
+            this.labelProcent.Location = new System.Drawing.Point(367, 275);
+            this.labelProcent.Name = "labelProcent";
+            this.labelProcent.Size = new System.Drawing.Size(21, 13);
+            this.labelProcent.TabIndex = 2;
+            this.labelProcent.Text = "0%";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 248);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loadingbarTimer
+            // 
+            this.loadingbarTimer.Tick += new System.EventHandler(this.LoadingbarTimer_Tick);
+            // 
+            // LoadingScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(684, 326);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelProcent);
+            this.Controls.Add(this.labelLoadingText);
+            this.Controls.Add(this.progressBarLoading);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingScreen";
+            this.Text = "ProNatur-Biomarkt-GmbH";
+            this.Load += new System.EventHandler(this.LoadingScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBarLoading;
+        private System.Windows.Forms.Label labelLoadingText;
+        private System.Windows.Forms.Label labelProcent;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer loadingbarTimer;
+    }
+}
+

ade2dbc0c63f09a11d4ad310b65407b12a4af220
NicoleZi <colle888@aol.com>
2024-02-01 10:19:35 +0100

    insert Database in project + two products in database

--- ProNaturBioMarktGmbH/ProductsScreen.cs
+++ ProNaturBioMarktGmbH/ProductsScreen.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,24 @@ namespace ProNaturBioMarktGmbH
 {
     public partial class ProductsScreen : Form
     {
+        private SqlConnection connectionDatabase = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Nicole\Documents\ProNatur Bimarkt GmbH.mdf;Integrated Security = True; Connect Timeout = 30");
+
         public ProductsScreen()
         {
             InitializeComponent();
+
+            connectionDatabase.Open();
+
+            string query = "select * from Products";
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(query, connectionDatabase);
+
+            DataSet dataSet = new DataSet();
+            sqlAdapter.Fill(dataSet);
+
+            dataGridViewProduct.DataSource = dataSet.Tables[0];
+            dataGridViewProduct.Columns[0].Visible = false;
+
+            connectionDatabase.Close();
         }
 
         private void ButtonProductSave_Click(object sender, EventArgs e)
